70b39ec08f93df8452225c2ef33d2750ce4c5e34
Patrick <patrickdalton811@gmail.com>
2018-06-04 19:50:01 +0000

    Defining some things

--- Paint_program.py
+++ Paint_program.py
@@ -1,1 +1,8 @@
+import RPI_functions_for_paint
+from gpiozero import Button, LED, PWMLED
+from time import *
+
+Red_button == Button(21)
+Green_button == Button(25)
+Blue_button == Button(18)
 
---
+++ RPI_functions_for_paint.py
@@ -0,0 +1,8 @@
+from gipozero import Button
+Red_button == Button(21)
+Green_button == Button(25)
+Blue_button == Button(18)
+
+def Sense_button_pressed():
+    """This function finds whether the red, blue, or green buttons were pressed and relays that to Arcade"""
+    Red_button.when_pressed
