2d60ced53a9a0139626fb19ae4e7d0b81e7897ba
SorryNotSorryBuddy <39745912+SorryNotSorryBuddy@users.noreply.github.com>
2018-06-04 12:48:44 -0700

    Remastered version of code, only has 2 functions now.

--- game.py
+++ game.py
@@ -1,108 +1,77 @@
-"""
-Sprite Collect Coins
+""" paint.py 
+    a simple paint program"""
 
-Simple program to show basic sprite usage.
+import pygame
 
-Artwork from http://kenney.nl
-
-If Python and Arcade are installed, this example can be run from the command line with:
-python -m arcade.examples.sprite_collect_coins
-"""
-
-import random
-import arcade
-import os
-
-# --- Constants ---
-SPRITE_SCALING_PLAYER = 0.5
-SPRITE_SCALING_COIN = 0.2
-COIN_COUNT = 50
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
 red = 0
 blue = 0
 green = 0
 
-class MyGame(arcade.Window):
-    """ Our custom Window Class"""
+def checkKeys(myData):
+    """test for various keyboard inputs"""
+    global red
+    global green
+    global blue
 
-    def __init__(self):
-        """ Initializer """
-        # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
-
-        # Set the working directory (where we expect to find files) to the same
-        # directory this .py file is in. You can leave this out of your own
-        # code, but it is needed to easily run the examples using "python -m"
-        # as mentioned at the top of this program.
-        file_path = os.path.dirname(os.path.abspath(__file__))
-        os.chdir(file_path)
-
-        # Variables that will hold sprite lists
-        self.player_list = None
-
-
-        # Set up the player info
-        self.player_sprite = None
-        self.score = 0
-
-        # Don't show the mouse cursor
-        self.set_mouse_visible(False)
-
-        arcade.set_background_color(arcade.color.WHITE)
-
-    def setup(self):
-        """ Set up the game and initialize the variables. """
-
-        # Sprite lists
-        self.player_list = arcade.SpriteList()
-
-        # Score
-
-
-        # Set up the player
-        # Character image from kenney.nl
-        self.player_sprite = arcade.Sprite("images/character.png", SPRITE_SCALING_PLAYER)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
-        self.player_list.append(self.player_sprite)
-
-
-    def on_draw(self):
-        """ Draw everything """
-        arcade.start_render()
-
-        self.player_list.draw()
-
-        # Put the text on the screen.
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        """ Handle Mouse Motion """
-
-        # Move the center of the player sprite to match the mouse x, y
-        self.player_sprite.center_x = x
-        self.player_sprite.center_y = y
-		
-		
-    def update(self, delta_time):
-        """ Movement and game logic """
-
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
-
-        # Generate a list of all sprites that collided with the player.
-
-
-        # Loop through each colliding sprite, remove it, and add to the score.
+    #extract the data
+    (event, background, drawColor, lineWidth, keepGoing) = myData
+    #print myData
+    if event.key == pygame.K_a:
+        #red
+        red = red + 15
+        drawColor = (red, green, blue)
+        if red == 255 :
+            red = 0
+        
+    elif event.key == pygame.K_s:
+        #green 
+        green = green + 15
+        drawColor = (red, green, blue)
+        if green == 255 :
+            green = 0
+            
+    elif event.key == pygame.K_d:
+        #blue
+        blue = blue + 15
+        drawColor = (red, green, blue)
+        if blue == 255 :
+            blue = 0
+    
+    #return all values 
+    myData = (event, background, drawColor, lineWidth, keepGoing)
+    return myData
 
 def main():
-    """ Main method """
-    window = MyGame()
-    window.setup()
-    arcade.run()
-    arcade.draw_point(x, y, arcade.color.RED, 100)
-    arcade.finish_render()
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption("Paint Program")
+    
+    background = pygame.Surface(screen.get_size())
+    background.fill((255, 255, 255))
+    
+    clock = pygame.time.Clock()
+    keepGoing = True
+    lineStart = (0, 0)
+    drawColor = (0, 0, 0)
+    lineWidth = 10
+    
+    while keepGoing:
+        clock.tick(30)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                keepGoing = False
+            elif event.type == pygame.MOUSEMOTION:
+                lineEnd = pygame.mouse.get_pos()
+                if pygame.mouse.get_pressed() == (1, 0, 0):
+                    pygame.draw.line(background, drawColor, lineStart, lineEnd, lineWidth)
+                lineStart = lineEnd
+            elif event.type == pygame.KEYDOWN:
+                myData = (event, background, drawColor, lineWidth, keepGoing)
+                myData = checkKeys(myData)
+                (event, background, drawColor, lineWidth, keepGoing) = myData
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
