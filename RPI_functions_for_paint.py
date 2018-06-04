b4638e2cce37dd4673c49add20621dcfbda0c14d
Patrick <patrickdalton811@gmail.com>
2018-06-04 20:30:04 +0000

    Made code run on its own

--- RPI_functions_for_paint.py
+++ RPI_functions_for_paint.py
@@ -1,8 +1,29 @@
-from gipozero import Button
+from gipozero import Button, LED, PWMLED
+
 Red_button == Button(21)
 Green_button == Button(25)
 Blue_button == Button(18)
+Red_light == LED(22)
+Green_light == LED(6)
+Blue_light == LED(19)
 
-def Sense_button_pressed():
-    """This function finds whether the red, blue, or green buttons were pressed and relays that to Arcade"""
-    Red_button.when_pressed
+def Sense_button_pressed(color):
+    """Finds whether the red, blue, or green buttons were pressed and relays that to Arcade"""
+    if color = "Red":
+        return Red
+    elif color = "Green":
+        return Green
+    elif color = "Blue":
+        return Blue
+
+def Brighten_and_dim_lights(Red_light, Green_light, Blue_light, color_to_change):
+    """Takes inputs from pygame determining what needs to be brightened or dimmed and gives that to the RGB LED"""
+    if color_to_change = Red_light:
+        if Red_light = 255:
+            
+    elif color_to_change = Green_light:
+
+    elif color_to_change = Blue_light:
+
+    
+    
